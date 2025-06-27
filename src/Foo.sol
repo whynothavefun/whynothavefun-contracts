0eb1ad48866cb0b610e6d462ef0d43016b82ff59
Cosmodude <vladislav.lenskii@gmail.com>
2025-06-27 11:12:56 +0900

    clena up; packages installed; rules added

--- src/Foo.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.29;
-
-contract Foo {
-    function echo(uint256 value) external pure returns (uint256) {
-        return value;
-    }
-}
